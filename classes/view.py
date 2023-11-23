6c1d88b129e9c5d3107a909d5c6b4572264e3a24
Doruk Zencirci <dorukzencirci@Doruks-MacBook-Pro-2.local>
2023-11-23 19:50:20 +0300

    roomView and dayView implementation changed

--- classes/view.py
+++ classes/view.py
@@ -19,37 +19,35 @@ class View:
 
     # start, end parameters are ignored due to pdf
     def roomView(self, start, end):
+        queryRes = []
         rooms = {}
+        #Getting results from all queries  
         for query in self.queryList:
-            # for all querys' organizations
-
-            # if not demanding rect based query
-            if query.room is not None:
-                # if room is reserved
-                if query.organization.getRoom(query.room.getId())[1] is not None:
-                    # append to given room name key's value in dict
-                    rooms[query.room.getName()] += query.organization.query(
-                        query.title, query.category, rect=None, room=query.room
-                    )
+            queryRes.extend(query.organization.query(
+                query.title, query.category, rect=query.rect, room=query.room
+            ))
+        # Grouping (event,room,start) tuples by room and adding to the rooms dictionary    
+        for event,room,start in queryRes:
+            if room in rooms:
+                rooms[room.getName()].append(event)
+            else:
+                rooms[room.getName()] = [event]    
         return rooms
 
     # Assuming start and end are given for a specific date intervals
     # start : datetime.datetime
     # end : datetime.datetime
     def dayView(self, start, end):
+        queryRes = []
         days = {}
-        delta = timedelta(days=1)
-        tempStart = start
-        while tempStart <= end:
-            days[tempStart.date()] = []
-
-            for query in self.queryList:
-                queryRes = query.organization.query(
-                    query.title, query.category, rect=query.rect, room=query.room
-                )
-                for res in queryRes:
-                    if res[2].date() == tempStart.date():
-                        days[tempStart.date()].append(res)
-
-            tempStart += delta
-        return days
+        #Getting results from all queries
+        for query in self.queryList:
+            queryRes.extend(query.organization.query(
+                query.title, query.category, rect=query.rect, room=query.room
+            ))
+        # Grouping (event,room,start) tuples by day and adding to the days dictionary
+        for res in queryRes:
+            if res[2].date() not in days:
+                days[res[2].date()].append(res)
+            else:                   
+                days[res[2].date()] = res
